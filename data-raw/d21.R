3a96d58d3242bdf2f7fc83c40b763b4bec09b404
context-dependent <tomas.k.mcmanus@gmail.com>
2024-08-29 15:47:42 -0400

    path fixes

--- data-raw/d21.R
+++ data-raw/d21.R
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:276319877480febe42c0307ba2ba62d8983a76b6267b61dbecb804a5a1114344
-size 366
+library(f.censuspumf)
+
+fetch_pumf("data-raw/d2021")
+d <- read_data("data-raw/cpumf2021/data_donnees_2021_ind_v2.csv", "data-raw/cpumf2021/cmd-files/IPUMF_2021_final_prog_en_v2.sas")
+
+records <- d$data
+var_labels <- d$var_labels
+
+usethis::use_data(records, overwrite = TRUE)
+usethis::use_data(var_labels, overwrite = TRUE)
